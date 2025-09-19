8d6800b773156d7ccb9495b5f87e253991456fa2
Wesley Lopes de Oliveira <x.wesleylopes@gmail.com>
2025-09-18 22:44:08 -0300

    fix: set cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: '*', // Allow requests from any origin
+    origin: ['http://localhost:3000', 'https://avmb-frontend.vercel.app'], // Allow requests from any origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
     credentials: true // Allow cookies to be sent
   })
